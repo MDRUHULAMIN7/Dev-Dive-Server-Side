0ae63a91570962e743cac4c9074686f291671c13
firoz.nur <firoznur5@gmail.com>
2024-09-19 18:29:15 +0600

    Add : User Post & Put

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
 // midleware
 
 app.use(cors());
 app.use(express.json());
 // mongodb
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aymctjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,38 +20,65 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-//   database collection
-const datbase = client.db("SoulTie");
-const usersCollection = datbase.collection('users')
+    //   database collection
+    const datbase = client.db("SoulTie");
+    const usersCollection = datbase.collection("users");
 
-// oparations
+    // oparations
 
 
 
-// ------------
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+      console.log(result);
+    });
+
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+
+      const userData = req.body;
+      const query = { email: email };
+      const update = {
+        $set: userData,
+      };
+      const result = await usersCollection.updateOne(query, update);
+      if (result.modifiedCount > 0) {
+        res.send({ message: "User updated successfully", result });
+      } else {
+        res.send({ message: "No changes made to the user", result });
+      }
+    });
+
+    // ------------
     await client.db("admin").command({ ping: 1 });
-    console.log(" Devdive successfully connected to MongoDB!");
+    console.log("Devdive successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-
   }
 }
 run().catch(console.dir);
 
-
-
 // mongodb
 
-app.get( '/' ,(req,res)=>{
-    res.send('Devdive is  running');
-})
+app.get("/", (req, res) => {
+  res.send("Devdive is  running");
+});
 
-app.listen(port,()=>{
-    console.log(`Devdive is running on:${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Devdive is running on:${port}`);
+});
