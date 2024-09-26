c921422dc0c201fc01317c3b152fede6151d2222
unknown <sanjidaema81@gmail.com>
2024-09-26 22:33:05 +0600

    my first commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -6,9 +8,11 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
 
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aymctjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,11 +24,17 @@ const client = new MongoClient(uri, {
   },
 });
 
+
 async function run() {
   try {
-    // Database collection
-    const database = client.db("DevDive");
-    const usersCollection = database.collection("users");
+    //   database collection
+    const datbase = client.db("DevDive");
+    const usersCollection = datbase.collection("users");
+    const blogsCollection = datbase.collection("blogs");
+
+
+    // oparations
+
 
     // Operations
     app.get("/users", async (req, res) => {
@@ -33,19 +43,24 @@ async function run() {
       res.send(result);
     });
 
+
     app.get("/user", async (req, res) => {
       const { email } = req.query;
       console.log(email);
 
+
       const query = { email: email };
       console.log(query);
+
 
       try {
         const user = await usersCollection.findOne(query);
 
+
         if (!user) {
           return res.status(404).json({ message: "User not found" });
         }
+
 
         res.json(user);
       } catch (error) {
@@ -54,10 +69,12 @@ async function run() {
       }
     });
 
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
       const query = { email: user.email };
+
 
       const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
@@ -68,9 +85,11 @@ async function run() {
       console.log(result);
     });
 
+
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
       console.log(email);
+
 
       const userData = req.body;
       const query = { email: email };
@@ -85,6 +104,52 @@ async function run() {
       }
     });
 
+
+
+
+
+
+    // get users from databse
+
+
+    app.get('/get-users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    // update-user-role
+    app.put(`/update-user-role/:email`, async (req, res) => {
+      const newRole = req.body.data
+      const { email } = req.params;
+      const query = { email: email }
+      const updateDoc = {
+        $set: {
+          role: newRole
+        }
+      }
+
+
+      const result = usersCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+
+    // post-blog
+
+
+    app.post('/post-blog', async (req, res) => {
+      const Info = req.body;
+      const result = await blogsCollection.insertOne(Info);
+      res.send(result)
+    })
+
+
+    // get blogs
+    app.get('/get-blog', async (req, res) => {
+      result = await blogsCollection.find().toArray();
+      res.send(result)
+    })
     // ------------
     await client.db("admin").command({ ping: 1 });
     console.log("DevDive successfully connected to MongoDB!");
@@ -94,11 +159,16 @@ async function run() {
 }
 run().catch(console.dir);
 
+
 // mongodb
 app.get("/", (req, res) => {
   res.send("DevDive is  running");
 });
 
+
 app.listen(port, () => {
   console.log(`DevDive is running on:${port}`);
 });
+
+
+
