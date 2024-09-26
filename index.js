276be93b04f90e5ec661c80361dc674beb2fc022
firoz.nur <firoznur5@gmail.com>
2024-09-26 18:49:31 +0600

    Add : Folder Structure For Nur

--- index.js
+++ index.js
@@ -28,111 +28,115 @@ async function run() {
     const blogsCollection = database.collection("blogs");
     const postsCollection = database.collection("posts");
 
+    // All Operations By Nur
+    // Import and use the separated route
+    const Nur = require("./Nur/Nur")(usersCollection);
+    app.use(Nur);
+
     // Operations
-    app.get("/users", async (req, res) => {
-      const cursor = usersCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    // app.get("/users", async (req, res) => {
+    //   const cursor = usersCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
 
-    app.get("/user", async (req, res) => {
-      const { email } = req.query;
-      console.log(email);
+    // app.get("/user", async (req, res) => {
+    //   const { email } = req.query;
+    //   console.log(email);
 
-      const query = { email: email };
-      console.log(query);
+    //   const query = { email: email };
+    //   console.log(query);
 
-      try {
-        const user = await usersCollection.findOne(query);
+    //   try {
+    //     const user = await usersCollection.findOne(query);
 
-        if (!user) {
-          return res.status(404).json({ message: "User not found" });
-        }
+    //     if (!user) {
+    //       return res.status(404).json({ message: "User not found" });
+    //     }
 
-        res.json(user);
-      } catch (error) {
-        console.error("Error fetching user:", error.message);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
-    });
+    //     res.json(user);
+    //   } catch (error) {
+    //     console.error("Error fetching user:", error.message);
+    //     res.status(500).json({ message: "Internal Server Error" });
+    //   }
+    // });
 
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      console.log(user);
-      const query = { email: user.email };
+    // app.post("/users", async (req, res) => {
+    //   const user = req.body;
+    //   console.log(user);
+    //   const query = { email: user.email };
 
-      const existingUser = await usersCollection.findOne(query);
-      if (existingUser) {
-        return res.send({ message: "user already exists", insertedId: null });
-      }
-      const result = await usersCollection.insertOne(user);
-      res.send(result);
-      console.log(result);
-    });
+    //   const existingUser = await usersCollection.findOne(query);
+    //   if (existingUser) {
+    //     return res.send({ message: "user already exists", insertedId: null });
+    //   }
+    //   const result = await usersCollection.insertOne(user);
+    //   res.send(result);
+    //   console.log(result);
+    // });
 
-    app.put("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      console.log(email);
+    // app.put("/users/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   console.log(email);
 
-      const userData = req.body;
-      const query = { email: email };
-      const update = {
-        $set: userData,
-      };
-      const result = await usersCollection.updateOne(query, update);
-      if (result.modifiedCount > 0) {
-        res.send({ message: "User updated successfully", result });
-      } else {
-        res.send({ message: "No changes made to the user", result });
-      }
-    });
+    //   const userData = req.body;
+    //   const query = { email: email };
+    //   const update = {
+    //     $set: userData,
+    //   };
+    //   const result = await usersCollection.updateOne(query, update);
+    //   if (result.modifiedCount > 0) {
+    //     res.send({ message: "User updated successfully", result });
+    //   } else {
+    //     res.send({ message: "No changes made to the user", result });
+    //   }
+    // });
 
-    app.post("/gitHubUsers", async (req, res) => {
-      const userData = req.body;
-      const query = { name: userData.name };
-      const existingUser = await usersCollection.findOne(query);
-      if (existingUser) {
-        res.send({ message: "User already exists", existingUser });
-      } else {
-        const result = await usersCollection.insertOne(userData);
-        res.send({ message: "User created successfully", result });
-      }
-    });
+    // app.post("/gitHubUsers", async (req, res) => {
+    //   const userData = req.body;
+    //   const query = { name: userData.name };
+    //   const existingUser = await usersCollection.findOne(query);
+    //   if (existingUser) {
+    //     res.send({ message: "User already exists", existingUser });
+    //   } else {
+    //     const result = await usersCollection.insertOne(userData);
+    //     res.send({ message: "User created successfully", result });
+    //   }
+    // });
 
-    app.get("/gitHubUsers/:username", async (req, res) => {
-      const username = req.params.username;
-      try {
-        const query = { name: username };
-        const user = await usersCollection.findOne(query);
+    // app.get("/gitHubUsers/:username", async (req, res) => {
+    //   const username = req.params.username;
+    //   try {
+    //     const query = { name: username };
+    //     const user = await usersCollection.findOne(query);
 
-        if (user) {
-          res.send({ user });
-        } else {
-          res.status(404).send({ message: "User not found" });
-        }
-      } catch (error) {
-        res.status(500).send({ message: "Error retrieving user", error });
-      }
-    });
+    //     if (user) {
+    //       res.send({ user });
+    //     } else {
+    //       res.status(404).send({ message: "User not found" });
+    //     }
+    //   } catch (error) {
+    //     res.status(500).send({ message: "Error retrieving user", error });
+    //   }
+    // });
 
-    app.put("/gitHubUsers/:username", async (req, res) => {
-      const username = req.params.username;
-      const loginData = req.body;
-      const query = { name: username };
+    // app.put("/gitHubUsers/:username", async (req, res) => {
+    //   const username = req.params.username;
+    //   const loginData = req.body;
+    //   const query = { name: username };
 
-      const update = {
-        $set: loginData,
-      };
+    //   const update = {
+    //     $set: loginData,
+    //   };
 
-      const result = await usersCollection.updateOne(query, update);
+    //   const result = await usersCollection.updateOne(query, update);
 
-      if (result.modifiedCount > 0) {
-        res.send({ message: "User login time updated successfully", result });
-      } else {
-        res.send({ message: "No changes made to the user login time", result });
-      }
-    });
-
+    //   if (result.modifiedCount > 0) {
+    //     res.send({ message: "User login time updated successfully", result });
+    //   } else {
+    //     res.send({ message: "No changes made to the user login time", result });
+    //   }
+    // });
 
     // get users from databse
 
@@ -189,12 +193,10 @@ async function run() {
           createdAt: new Date(), // Optional: To track when the post was created
         });
 
-        res
-          .status(200)
-          .json({
-            message: "Post added successfully",
-            postId: result.insertedId,
-          });
+        res.status(200).json({
+          message: "Post added successfully",
+          postId: result.insertedId,
+        });
       } catch (error) {
         console.error("Error adding post:", error);
         res.status(500).json({ message: "Failed to add post" });
