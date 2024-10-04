f74d29ad8df3ee62612b3a13cff7fc0c331fd782
firoz.nur <firoznur5@gmail.com>
2024-10-04 14:28:38 +0600

    Add : Get Conditional Likes

--- Nur/Leaderboard.js
+++ Nur/Leaderboard.js
@@ -19,25 +19,46 @@ module.exports = (postsCollection, likesCollection, commentsCollection) => {
 
   router.get("/leaderBoardLikes", async (req, res) => {
     try {
-      const result = await likesCollection
-        .aggregate([
-          {
-            $group: {
-              _id: "$email",
-              count: { $sum: 1 },
-              name: { $first: "$name" },
+      const { loadAllLikes } = req.query;
+      let likesQuery = likesCollection;
+      if (loadAllLikes !== "true") {
+        likesQuery = await likesQuery
+          .aggregate([
+            {
+              $group: {
+                _id: "$email",
+                count: { $sum: 1 },
+                name: { $first: "$name" },
+              },
             },
-          },
-          {
-            $sort: { count: -1 },
-          },
-          {
-            $limit: 5,
-          },
-        ])
-        .toArray();
+            {
+              $sort: { count: -1 },
+            },
+          ])
+          .toArray();
+      }
 
-      res.status(200).send(result);
+      if (loadAllLikes === "true") {
+        likesQuery = await likesQuery
+          .aggregate([
+            {
+              $group: {
+                _id: "$email",
+                count: { $sum: 1 },
+                name: { $first: "$name" },
+              },
+            },
+            {
+              $sort: { count: -1 },
+            },
+            {
+              $limit: 5,
+            },
+          ])
+          .toArray();
+      }
+
+      res.status(200).send(likesQuery);
     } catch (error) {
       console.error("Error fetching leaderBoard likes:", error);
       res.status(500).send({ message: "Failed to fetch leaderBoard likes" });
