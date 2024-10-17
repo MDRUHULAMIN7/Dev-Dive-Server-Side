680d02db9aed4e87e2ee2aba5f7019286eba337c
firoz.nur <firoznur5@gmail.com>
2024-10-17 19:18:29 +0600

    Post : Report Data

--- Nur/ReportData.js
+++ Nur/ReportData.js
@@ -2,7 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (reportDataCollection) => {
- 
+  router.post("/reportData", async (req, res) => {
+    try {
+      const reportData = req.body;
+      const { post_id, reportBy } = req.body;
+      console.log("reportData", reportData);
+      console.log("Received post_id:", post_id);
+      console.log("Received user email:", reportBy.email);
+
+
+
+
+
+      // Save the report data to your MongoDB collection
+      const result = await reportDataCollection.insertOne(reportData);
+
+      if (result.insertedId) {
+        return res
+          .status(200)
+          .json({ message: "Report submitted successfully." });
+      } else {
+        return res.status(500).json({ message: "Failed to submit report." });
+      }
+    } catch (error) {
+      console.error("Error saving report data:", error);
+      res.status(500).json({ message: "Failed to save report data." });
+    }
+  });
 
   return router;
 };
